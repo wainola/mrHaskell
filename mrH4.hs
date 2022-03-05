165f0b00476a5d452fe0fa99b1d889423c53aff0
Nicolas Riquelme <nrriquelme@gmail.com>
2022-03-05 18:19:27 -0300

    fooling around with maybe and strings

--- mrH4.hs
+++ mrH4.hs
@@ -55,3 +55,52 @@ replaceThe = (\x -> if x == "the" then "a" else x)
 replaceAndTransform str = case notThe str of
     (Just str) -> "bad word"
     Nothing -> unwords $ map replaceThe (words str)
+
+vocals = ['a', 'e', 'i', 'o', 'u']
+
+-- countBeforeVowel :: String -> Int -> Int
+countBeforeVowel str counter = let worded = words str
+                                   (h:t) = worded in
+                                       if h == "the" && head t !! 0 `elem` vocals
+                                           then
+                                               let uw = unwords t
+                                                   c = (+1) counter
+                                                in countBeforeVowel uw c
+                                            else
+                                                if h /= "the" && h == "the"
+                                                    then
+                                                        let uw = unwords t
+                                                        in countBeforeVowel uw counter
+                                                    else
+                                                        show counter
+
+extractVowels str = filter (\x -> x `elem` vocals) str
+extractConsonants str = filter (\x -> not (x `elem` vocals) && x /= ' ') str
+countVowels str = length $ extractVowels str
+
+newtype Word' = Word' String deriving (Eq, Show)
+
+mkWord :: String -> Maybe Word'
+mkWord str = let lengthVowesl = length $ extractVowels str
+                 lengthConsonants = length $ extractConsonants str
+                 in if lengthVowesl > lengthConsonants then Nothing else Just (Word' str)
+
+isJust :: Maybe a -> Bool
+isJust (Just a) = True
+isJust _ = False
+
+isNothing :: Maybe a -> Bool
+isNothing Nothing = True
+isNothing (Just a) = False
+
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee x func Nothing = x
+mayybee x func (Just a) = func a
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe (x:_) = Just x
+
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just a) = [a]
