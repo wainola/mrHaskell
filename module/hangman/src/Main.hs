3ea6f467fa818fb838426b6d8d6eb9149a9b9525
Nicolas Riquelme <nrriquelme@gmail.com>
2022-03-06 01:06:01 -0300

    silly game

---
+++ module/hangman/src/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import Data.List (intersperse)
+import System.Exit (exitSuccess)
+import System.IO (BufferMode(NoBuffering),
+                  hSetBuffering,
+                  stdout)
+import System.Random (randomRIO)
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
